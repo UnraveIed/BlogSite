6947736620fa62c77e0824cc245e18beb409c13a
Unravel <np_batuQhotmail.com>
2022-10-13 00:00:51 +0300

    Admin message controller added. Inbox page created.

---
+++ BlogSite/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MessageController : Controller
+    {
+        public IActionResult InBox()
+        {
+            return View();
+        }
+    }
+}
